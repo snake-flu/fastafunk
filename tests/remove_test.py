2a9ef2b4bce7b54ad3f3a1c8f7553985d7b12672
Rachel Colquhoun <rmnorris@well.ox.ac.uk>
2020-04-09 16:38:43 +0100

    fix remove tests

--- tests/remove_test.py
+++ tests/remove_test.py
@@ -10,13 +10,13 @@ data_dir = os.path.join(this_dir, 'tests', 'data', 'remove')
 class TestRemove(unittest.TestCase):
     def test_run_remove(self):
         in_fasta = ["%s/test_remove.fasta" %data_dir]
-        in_metadata = ["%s/test_filter.txt" %data_dir]
+        in_metadata = ["%s/test_filter.tsv" %data_dir]
         out_fasta = "%s/tmp.filtered.fasta" %data_dir
         log_file = "%s/tmp.filtered.fasta.log" %data_dir
         expected = "%s/filtered.fasta" %data_dir
         remove_fasta(in_fasta, in_metadata, out_fasta, log_file)
         self.assertTrue(filecmp.cmp(out_fasta, expected, shallow=False))
-        #os.unlink(out_fasta)
+        os.unlink(out_fasta)
 
         self.assertTrue(filecmp.cmp(out_fasta + ".log", expected + ".log", shallow=False))
-        #os.unlink(out_fasta + ".log")
+        os.unlink(out_fasta + ".log")
