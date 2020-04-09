6dd75f107a85d567b48f311deb09d97998819e0a
Rachel Colquhoun <rmnorris@well.ox.ac.uk>
2020-04-09 16:38:43 +0100

    fix remove tests

--- tests/merge_test.py
+++ tests/merge_test.py
@@ -30,12 +30,12 @@ class TestMerge(unittest.TestCase):
         self.assertTrue(filecmp.cmp(out_fasta, expected, shallow=False))
         os.unlink(out_fasta)
 
-    def test_run_merge_no_log_no_out_fasta(self):
-        in_fasta = glob.glob("%s/*.f*a" % data_dir)
-        in_metadata = ["%s/metadata.csv" % data_dir]
-        out_fasta = ""
-        log_file = None
-        expected = "%s/merged_fasta" % data_dir
-        merge_fasta(in_fasta, in_metadata, out_fasta, log_file)
-        self.assertTrue(filecmp.cmp(out_fasta, expected, shallow=False))
-        os.unlink(out_fasta)
+    # def test_run_merge_no_log_no_out_fasta(self):
+    #     in_fasta = glob.glob("%s/*.f*a" % data_dir)
+    #     in_metadata = ["%s/metadata.csv" % data_dir]
+    #     out_fasta = ""
+    #     log_file = None
+    #     expected = "%s/merged_fasta" % data_dir
+    #     merge_fasta(in_fasta, in_metadata, out_fasta, log_file)
+    #     self.assertTrue(filecmp.cmp(out_fasta, expected, shallow=False))
+    #     os.unlink(out_fasta)
