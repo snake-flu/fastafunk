f8c4b35263910f7e58aec825df485c4b65dee7d6
Xiaoyu Yu <57363085+xiaoyu518@users.noreply.github.com>
2020-04-15 12:02:18 +0100

    Add files via upload

--- fastafunk/subcommands/merge.py
+++ fastafunk/subcommands/merge.py
@@ -10,6 +10,7 @@ def run(options):
     merge_fasta(
         options.in_fasta,
         options.in_metadata,
+        options.index_column,
         options.out_fasta,
         options.log_file
     )
