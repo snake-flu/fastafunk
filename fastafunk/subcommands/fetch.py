f8a1a863a023192f44b2c2b5e593eae86192bde4
Rachel Colquhoun <rmnorris@well.ox.ac.uk>
2020-04-18 11:10:42 +0100

    add fetch

---
+++ fastafunk/subcommands/fetch.py
@@ -0,0 +1,18 @@
+"""
+This file is part of Fastafunk (https://github.com/cov-ert/fastafunk).
+Copyright 2020 Xiaoyu Yu (xiaoyu.yu@ed.ac.uk) & Rachel Colquhoun (rachel.colquhoun@ed.ac.uk).
+"""
+
+from fastafunk.fetch import *
+
+def run(options):
+
+    fetch_fasta(
+        options.in_fasta,
+        options.in_metadata,
+        options.index_column,
+        options.out_fasta,
+        options.out_metadata,
+        options.log_file,
+        options.header_delimiter
+    )
