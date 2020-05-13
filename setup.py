576e810606a47c747b74169a04249d916c9af515
rmcolq <rachel.colquhoun@ed.ac.uk>
2020-05-13 14:52:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastafunk",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     url="https://github.com/cov-ert/fastafunk",
     license="MIT",
