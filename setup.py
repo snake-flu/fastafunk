7d8368abe1832798cc338ff052fe7dcb8f78aa6d
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2021-07-14 20:12:15 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastafunk",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     url="https://github.com/cov-ert/fastafunk",
     license="MIT",
